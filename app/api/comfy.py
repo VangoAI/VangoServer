02f06561aaaae03f13f0a9063f1eca0e6b8e56ce
Rajen Dey <rajen.dey@berkeley.edu>
2023-08-14 23:24:49 -0700

    remove auth from /comfy/server

--- app/api/comfy.py
+++ app/api/comfy.py
@@ -4,6 +4,5 @@ from app.utils import token_required
 comfy = Blueprint('comfy', __name__)
 
 @comfy.route('/server')
-@token_required
 def get_server(user_id):
     return {'api_url': 'https://server1.vango.ai', 'ws_url': 'wss://server1.vango.ai/ws'}, 200
